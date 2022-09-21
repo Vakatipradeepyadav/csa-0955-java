96c10fea0974284ee9a0cff8ee7d2f54178af3c8
Vakatipradeepyadav <112741292+Vakatipradeepyadav@users.noreply.github.com>
2022-09-21 19:03:54 +0530

    Add files via upload

---
+++ leap.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class leap {
+ public static void main(String[] args) {
+ Scanner sc = new Scanner(System.in);
+ System.out.print("Enter the Year: ");
+ int year = sc.nextInt();
+ if((year%400==0) || ((year%4==0) && (year%100!=0))){
+ System.out.println(year+" is a Leap Year.");
+ }
+ else {
+ System.out.println(year+" is non Leap Year.");
+ }
+ }
+}
